f8ae2bd844dabfe478d2d32b89221a7cbffb648f
김봉현 <85615745+BongHyun-Kim@users.noreply.github.com>
2022-01-20 10:31:15 +0900

    map edit

--- lib/Screen/MapScreen.dart
+++ lib/Screen/MapScreen.dart
@@ -1,4 +1,4 @@
-import 'dart:html';
+
 
 import 'package:flutter/material.dart';
 import 'package:flutter_kakao_map/flutter_kakao_map.dart';
@@ -6,7 +6,6 @@ import 'package:flutter_kakao_map/kakao_maps_flutter_platform_interface.dart';
 import '../other/NavBar.dart';
 import '../other/UpperBar.dart';
 import '../other/BottomBar.dart';
-import 'package:location/location.dart';
 
 
 void main(){
@@ -19,40 +18,11 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late double lat;
-  late double lng;
-  Location location = new Location();
-  late bool _serviceEnabled;
-  late PermissionStatus _permissionGranted;
-
-  _locationMe() async{
-    _serviceEnabled = await location.serviceEnabled();
-    if (!_serviceEnabled){
-      _serviceEnabled = await location.requestService();
-      if (!_serviceEnabled){
-        return;
-      }
-    }
-    _permissionGranted = await location.hasPermission();
-    if(_permissionGranted == PermissionStatus.denied){
-      _permissionGranted = await location.requestPermission();
-      if(_permissionGranted != PermissionStatus.granted){
-        return;
-      }
-    }
-    location.onLocationChanged.listen((res) {
-      setState(() {
-        lat = res.latitude;
-        lng = res.longitude;
-      });
-    });
-  }
-
 
   late KakaoMapController mapController;
-  MapPoint _visibleRegion = MapPoint(lat, lng);
+  MapPoint _visibleRegion = MapPoint(37.5087553, 127.0632877);
   CameraPosition _kInitialPosition =
-  CameraPosition(target: MapPoint(35.020396, 126.793590), zoom: 5);
+  CameraPosition(target: MapPoint(37.5087553, 127.0632877), zoom: 5);
 
   void onMapCreated(KakaoMapController controller) async {
     final MapPoint visibleRegion = await controller.getMapCenterPoint();
@@ -61,6 +31,7 @@ class _MyAppState extends State<MyApp> {
       _visibleRegion = visibleRegion;
     });
   }
+
 
   @override
   Widget build(BuildContext context) {
